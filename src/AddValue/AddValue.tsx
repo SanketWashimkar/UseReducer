6840aa6da4fbe77c41d8a25146d405d1d346e495
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-09 15:47:46 +0530

    main:4th commit

---
+++ src/AddValue/AddValue.tsx
@@ -0,0 +1,64 @@
+import { useReducer } from "react";
+
+type stateType = {
+    username: 'ARC'
+  counter: number;
+  status: 'increment' | 'decrement' | 'start' ;
+};
+
+
+type actionType = {type: string};
+const reducerFun = (state:stateType, action:actionType)=>{
+    console.log('action' , action)
+    switch (action.type) {
+        case "increment":
+            return {
+       ...state,
+       counter:state.counter + 1,
+       status:action.type
+            };
+          case "decrement":
+            return {
+       ...state,
+       counter:state.counter - 1,
+       status:action.type
+            };
+    
+        default:
+  return state;
+    }
+}
+
+
+
+
+const intialState : stateType = {counter:1,status:"start", username:"ARC" };
+
+const AddValue = () => {
+
+    const [state, dispatch] = useReducer(reducerFun,intialState); 
+
+return (
+<div>
+    username:{state.username}
+    <br/>
+
+<p> counter : {state.counter}</p>
+<br/>
+
+<p> status : {state.status}</p>
+<br/>
+<button type="button" onClick={() =>{dispatch({type:"increment"});
+}} > Increment</button>
+<br/>
+
+<button type="button" onClick={() =>{dispatch({type:"decrement"});
+}}>Decrement</button>
+
+</div>
+   
+);
+
+}
+
+export default AddValue;
