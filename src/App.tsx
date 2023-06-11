601e1617aae37cb43cecf015ac2c371a01fc40bc
SanketWashimkar <133958334+SanketWashimkar@users.noreply.github.com>
2023-06-11 12:22:53 +0530

    main:10th commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 import UserInfo from './component/UserInfo/UserInfo';
+import UseRef from './component/UseRef/UseRef';
 
 
 function App() {
   return (
    <div>
   <UserInfo />
+  <UseRef />
    </div>
   );
 }
